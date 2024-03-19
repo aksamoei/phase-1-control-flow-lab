9b96bba05db96f52e25d22625537971316bc577e
Abraham <abukip344@gmail.com>
2024-03-19 11:07:38 -0400

    conditional tests

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message;
+  if (distance <= 400) {
+    message = "This one is on me!";
+  }
+  else if (distance <= 2000) {
+    message = "That will be twenty bucks.";
+  }
+  else if (distance <= 2500) {
+    message = "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500) {
+    message = "No can do.";
+  }
+  return message
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return (cityName === "NYC") ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let gratitude;
+  switch (tip) {
+    case "generous":
+      gratitude = "Thank you so much.";
+      break;
+    case "not as generous":
+      gratitude = "Thank you.";
+      break;
+    default:
+      gratitude = "Bye.";
+  }
+  return gratitude;
 }
+
+
